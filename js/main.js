2e3f08e4778532e9497dc1d6388f1e67382b11b7
takashiono2 <takashiono710@gmail.com>
2021-07-04 21:43:07 +0900

    初級09文字を反転させてみよう

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    random: 'fun'
-    // random: 'lazy'
+    greet: 'Hello,ninjacode!'
+  },
+  methods: {
+    reverseText: function() {
+      this.greet = this.greet.split("").reverse().join("");
+    }
   }
 });
+
+// No.09
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     greet: 'Hello,ninjacode!'
+//   },
+//   methods: {
+//     reverseText: function() {
+//       this.greet = this.greet.split("").reverse().join("");
+//     }
+//   }
+// });
+
+// No.07
+// var app = new Vue({
+//   el: '#app',
+//   methods: {
+//     alertText: function() {
+//       alert("I'm Ninja!!")
+//     }
+//   }
+// });
+
+// No.06
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     random: 'fun'
+//     // random: 'lazy'
+//   }
+// });
 
 // No.04
 // var app = new Vue({
